839925eff58d3ffc68c16781b655f7d56e6b8d6f
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2024-04-07 21:58:51 +0300

    Update image size

--- src/components/molecules/privateGames/privateEventHero2.tsx
+++ src/components/molecules/privateGames/privateEventHero2.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const PrivateEventHero2 = () => {
   return (
-    <div className="relative flex flex-col md:flex-row bg-[#7da9d3] items-end w-full items-center justify-end h-[300px] md:h-[500px] ">
+    <div className="relative flex flex-col md:flex-row bg-[#7da9d3] items-end w-full items-center justify-end h-[300px] md:h-[500px] xl:h-[700px]">
       <Image
         className="w-full h-[300px] md:h-full object-cover object-center"
         src="/private-hero.webp"
